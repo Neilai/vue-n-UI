b54d804727d33b8397a53bf1899624414cb90179
Neilai <1131894367@qq.com>
2018-07-03 23:36:16 +0800

    完善发布button

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Neil
+ * 2018-07-03 14:15
+ */
+import Button from './src/button.vue'
+import ButtonGroup from './src/button-group.vue'
+import Icon from './src/icon.vue'
+
+export {Button, ButtonGroup,Icon}
